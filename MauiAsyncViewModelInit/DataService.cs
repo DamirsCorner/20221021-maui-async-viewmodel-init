f5e98b681f9dc9ffd7024603b9d07bf1e91e4564
Damir Arh <damir.arh@gmail.com>
2022-10-09 12:22:49 +0200

    Blocking async calls in constructor

---
+++ MauiAsyncViewModelInit/DataService.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAsyncViewModelInit;
+public class DataService
+{
+    public async Task<List<string>> LoadItems()
+    {
+        await Task.Delay(5000).ConfigureAwait(false); // prevent deadlock
+        return Enumerable.Range(1, 10).Select(i => $"Item {i}").ToList();
+    }
+}
---
+++ MauiAsyncViewModelInit/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiAsyncViewModelInit;
+
+[ObservableObject]
+public partial class MainViewModel
+{
+    [ObservableProperty]
+    private List<string> items = new();
+
+    public MainViewModel(DataService dataService)
+    {
+        items = dataService.LoadItems().Result;
+    }
+}
