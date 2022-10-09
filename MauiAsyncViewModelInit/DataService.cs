f2e5cc27eeba313aca3be93fb78e35c2cdd96352
Damir Arh <damir.arh@gmail.com>
2022-10-09 12:32:34 +0200

    Data load failure crashes the application

--- MauiAsyncViewModelInit/DataService.cs
+++ MauiAsyncViewModelInit/DataService.cs
@@ -1,9 +1,16 @@
 ﻿namespace MauiAsyncViewModelInit;
 public class DataService
 {
+    private bool fail = true;
+
     public async Task<List<string>> LoadItems()
     {
         await Task.Delay(5000).ConfigureAwait(false); // prevent deadlock
+        if (fail)
+        {
+            fail = false;
+            throw new Exception("Failed to load items");
+        }
         return Enumerable.Range(1, 10).Select(i => $"Item {i}").ToList();
     }
 }
